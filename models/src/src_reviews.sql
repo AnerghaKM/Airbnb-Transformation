79b301ee2beb86a13d0ad5381ff7853631c0faf1
anergha.mohan@ocean.ibm.com <anergha.mohan@ocean.ibm.com>
2023-01-21 09:05:26 +0000

    commit

---
+++ models/src/src_reviews.sql
@@ -0,0 +1,10 @@
+with raw_reviews as (
+    select * from AIRBNB.RAW.RAW_REVIEWS
+)
+select 
+LISTING_ID NUMBER,
+	DATE as review_date,
+	REVIEWER_NAME ,
+	COMMENTS as review_text,
+	SENTIMENT as review_sentiment
+from raw_reviews
